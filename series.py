7fc5bbab7a9687a5a471b8d32b7ff958c5fe2ade
MonikaChris <monidavies@gmail.com>
2022-10-05 20:45:48 -0700

    Adds sum_series function

--- series.py
+++ series.py
@@ -13,8 +13,10 @@ def fibonacci(n):
 
 def fib_iter(n):
     """
-       This function returns the fibonacci number at the index provided
-        """
+    This function returns the fibonacci number at the index provided
+    :param n: index
+    :return: value at index n
+    """
     if n == 0:
         return 0
 
@@ -35,6 +37,8 @@ def fib_iter(n):
 def lucas(n):
     """
     This function returns the lucas number at the index provided
+    :param n: index
+    :return: value at index n
     """
     if n < 0:
         return -1
@@ -47,3 +51,22 @@ def lucas(n):
 
     return lucas(n-1) + lucas(n-2)
 
+
+def sum_series(n, idx_zero = 0, idx_one = 1):
+    """
+    This function returns the element at position n of a sequence,
+    where each element in the sequence is the sum of the previous
+    two elements. Optionally takes 2 starting values.
+    :param n: index to be returned
+    :param idx_zero: value at index 0 (default value 0)
+    :param idx_one: value at index 1 (default value 1)
+    :return: value at index n
+    """
+
+    if n == 0:
+        return idx_zero
+
+    if n == 1:
+        return idx_one
+
+    return sum_series(n - 1, idx_zero, idx_one) + sum_series(n - 2, idx_zero, idx_one)
--- test_series.py
+++ test_series.py
@@ -2,6 +2,7 @@ import pytest
 from series import fibonacci
 from series import fib_iter
 from series import lucas
+from series import sum_series
 
 # Fibonacci Tests
 def test_fib_zero():
@@ -85,3 +86,29 @@ def test_lucas_five():
 
 def test_lucas_seven():
     assert lucas(7) == 29
+
+
+# Sum_series Tests
+
+def test_default_start_zero():
+    assert sum_series(0) == 0
+
+
+def test_default_start_one():
+    assert sum_series(1) == 1
+
+
+def test_default_start_six():
+    assert sum_series(6) == 8
+
+
+def test_lucas_start_four():
+    assert sum_series(4, 2, 1) == 7
+
+
+def test_negative_start_four():
+    assert sum_series(4, -1, -2) == -8
+
+
+def test_advanced_start_four():
+    assert sum_series(4, 10, 11) == 53
