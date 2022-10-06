9ac9c2a1c16fe2c89f87fa5507708f93e61fbe62
MonikaChris <monidavies@gmail.com>
2022-10-05 20:15:58 -0700

    Adds iterative fibonacci and lucas functions

--- series.py
+++ series.py
@@ -1,8 +1,49 @@
-def fibonacci(num):
-    if num == 0:
+def fibonacci(n):
+    """
+    This function returns the fibonacci number at the index provided
+     """
+    if n == 0:
         return 0
 
-    if num == 1:
+    if n == 1:
         return 1
 
-    return fibonacci(num - 1) + fibonacci(num - 2)
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+def fib_iter(n):
+    """
+       This function returns the fibonacci number at the index provided
+        """
+    if n == 0:
+        return 0
+
+    if n == 1:
+        return 1
+
+    idx = 2
+    first = 0
+    second = 1
+    total = 1
+    while idx <= n:
+        total = first + second
+        first = second
+        second = total
+        idx += 1
+    return total
+
+def lucas(n):
+    """
+    This function returns the lucas number at the index provided
+    """
+    if n < 0:
+        return -1
+
+    if n == 0:
+        return 2
+
+    if n == 1:
+        return 1
+
+    return lucas(n-1) + lucas(n-2)
+
--- test_series.py
+++ test_series.py
@@ -1,27 +1,87 @@
 import pytest
 from series import fibonacci
+from series import fib_iter
+from series import lucas
 
-def test_zero():
+# Fibonacci Tests
+def test_fib_zero():
     actual = fibonacci(0)
     expected = 0
     assert actual == expected
 
-def test_one():
+def test_fib_one():
     actual = fibonacci(1)
     expected = 1
     assert actual == expected
 
-def test_two():
+def test_fib_two():
     actual = fibonacci(2)
     expected = 1
     assert actual == expected
 
-def test_five():
+def test_fib_five():
     actual = fibonacci(5)
     expected = 5
     assert actual == expected
 
-def test_seven():
+def test_fib_seven():
     actual = fibonacci(7)
     expected = 13
     assert actual == expected
+
+
+# Fib_iter Tests
+def test_fib_iter_zero():
+    actual = fib_iter(0)
+    expected = 0
+    assert actual == expected
+
+
+def test_fib_iter_one():
+    actual = fib_iter(1)
+    expected = 1
+    assert actual == expected
+
+
+def test_fib_iter_two():
+    actual = fib_iter(2)
+    expected = 1
+    assert actual == expected
+
+
+def test_fib_iter_five():
+    actual = fib_iter(5)
+    expected = 5
+    assert actual == expected
+
+
+def test_fib_iter_seven():
+    actual = fib_iter(7)
+    expected = 13
+    assert actual == expected
+
+# Lucas Tests
+
+
+def test_lucas_zero():
+    actual = lucas(0)
+    expected = 2
+    assert actual == expected
+
+
+def test_lucas_one():
+    actual = lucas(1)
+    expected = 1
+    assert actual == expected
+
+
+def test_lucas_two():
+    assert lucas(2) == 3
+
+
+def test_lucas_five():
+    assert lucas(5) == 11
+
+
+def test_lucas_seven():
+    assert lucas(7) == 29
